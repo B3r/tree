1e5623afd2c7a8ce4353c84633e1f3926e60f573
MF <Marvin.Flock@rwth-aachen.de>
2018-12-14 21:27:04 +0100

    project init

---
+++ src/test/java/it/marvinflock/Tree/TreeApplicationTests.java
@@ -0,0 +1,17 @@
+package it.marvinflock.Tree;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
